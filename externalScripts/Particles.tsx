4699083ba5904a18e74bed2606efd7fdc495908a
backek_p <backek_p@etna-alternance.net>
2022-12-23 23:56:22 +0100

    commit 3

---
+++ externalScripts/Particles.tsx
@@ -0,0 +1,15 @@
+import Script from 'next/script'
+
+const Particles = () => {
+  return (
+    <>
+      <Script 
+      src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js" 
+      />
+      <Script 
+      src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js" 
+      />
+    </>
+  )
+}
+export default Particles
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,18 @@ import { Canvas } from "@react-three/fiber";
 import LightBulb from '../compoments/LightBulb';
 import Box from '../compoments/Box';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { gsap } from "gsap";
+import Link from 'next/link';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 export default function Home() {
+  AOS.init();
+  const [hoverMenu, setHoverMenu] = useState(true);
+  const [clickMenu, setClickMenu] = useState(true);
+
+  const [hoverclose, setHoverclose] = useState(true);
+
   const [scroll, setScroll] = useState(42);
   const [scrollrotate, setScrollrotate] = useState(0);
 
@@ -42,8 +51,57 @@ export default function Home() {
 
       </Canvas>
       <div className={styles.page}>
-      </div>
+        <header>
+          <div className={styles.logo}>
+            <Link href={'#'}>PULSY</Link>
+          </div>
+          {/* menu burger */}
+          <div className={styles.menu} onMouseOver={()=>{
+              setHoverMenu((prevHoverMenu) => !prevHoverMenu)
+            }} onClick={()=>{
+              setClickMenu((prevClickMenu) => !prevClickMenu)
+            }}>
+            <div className={styles.menu1}></div>
+            <div className={styles.menu1}></div>
+            <div className={styles.menu2} style={{width: hoverMenu ? "30px" : "50px"} }></div>
+          </div>
+          {/* menu  */}
+          <div className={styles.menuderou} style={{width: clickMenu ? "0vw" : "50vw"}}>
 
+            <div className={styles.close} onMouseOver={()=>{
+              setHoverclose((prevHoverclose) => !prevHoverclose)
+            }} onClick={()=>{
+              setClickMenu((prevClickMenu) => !prevClickMenu)
+            }} style={{display: clickMenu ? "none" : "block"}}>
+              <div className={styles.close1} style={{width: hoverclose ? "50px" : "30px"}}></div>
+              <div className={styles.close2} style={{width: hoverclose ? "50px" : "30px"}}></div>
+
+            </div>
+            <ul>
+              <li><Link href={'#'}>A propos de Moi</Link></li>
+              <li><Link href={'#'}>Mes Projets</Link></li>
+              <li><Link href={'#'}>Me contacter</Link></li>
+            </ul>
+
+          </div>
+        </header>
+        <section className={styles.title}>
+          <div className={styles.title1}>
+            <div>
+              <h1>Pulsy BACKEKOLO</h1>
+              <h2>Developpeur Web Front - End </h2>
+            </div>
+            <h3>
+              <strong>{"<  "}</strong>
+               Hello World, 
+            Bienvenue sur mon portfolio ! <br />
+            Vous pouvez en <Link href={'#'}>apprendre un peu plus</Link> sur moi ou <Link href={'#'}>découvrir mes projets</Link>. <strong>{" >"}</strong>
+            </h3>
+          </div>
+
+        </section>
+      </div>
+           
   </div>    
    
    </>
