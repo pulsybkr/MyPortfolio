0b30173d279958318d95554adaccd2c38dd5b5d5
backek_p <backek_p@etna-alternance.net>
2023-01-18 12:16:53 +0100

    commit du id en commentaire 1

--- pages/projet/[id].tsx
+++ pages/projet/[id].tsx
@@ -2,73 +2,39 @@
 /* eslint-disable @next/next/no-img-element */
 import data from '../../fichier.json'
 import { useRef, useEffect, useState } from 'react';
-import styles from '../../styles/Project.module.css'
 import React, { Component } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import { useRouter } from 'next/router'
-import Header from '../../compoments/header';
-// Import Swiper
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import { EffectCoverflow, Pagination, Virtual } from "swiper";
-import Link from 'next/link';
-import Souris from '../../compoments/sourisperso';
+import router, { useRouter } from 'next/router'
 
-function Page() {
+
+function Page({ post }) {
   const router = useRouter();
-  const id = String(router.query.id)
-
-  const nombr = Number(id)
-  const post = data[nombr]
-
-  // const slides = post.slide
-  console.log(typeof id)
   useEffect(()=>{
-    // aos animation
-    AOS.init();
-      
+    AOS.init(); // initialize AOS animation
   }, [])
   return(
     <>
-      {/* <section className={styles.sectionprojet}>
-        <Header/>
-      <Souris/>
-
-        <main>
-          <div className={styles.titre}>
-              <h1>{post.name}</h1>
-          </div>
-          <div className={styles.bkg}>
-            <img src={post.image} alt="image de presentation" />
-          </div>
-          <div className={styles.description}>
-            <div className={styles.techno}>
-              <h2>Résumé</h2>
-              <p>{post.description}</p>
-            </div>
-            <div className={styles.techno}>
-              <h2>Techno</h2>
-              <p>{post.techno}</p>
-            </div>
-
-            
-          </div>
-          
-          <div className={styles.voir}>
-          <Link href={`${post.lien}`} target="_blank"><div>
-                <h2>
-                  Voir en Direct
-                </h2>
-              </div></Link>
-              
-          </div>
-        </main>
-      </section> */}
+        <h1>{post.title}</h1>
     </>
   )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: data.map((post) => ({ params: { id: post.id.toString() } })),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const {id} = params
+  const post = data[id]
+  return {
+    props: {
+      post: post
+    }
+  }
 }
 
 export default Page;
