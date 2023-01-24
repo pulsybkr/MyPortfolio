5cb4bf6559d9f914f8f98c617b608dc4828b9c41
backek_p <backek_p@etna-alternance.net>
2023-01-24 12:06:41 +0100

    renvoi d

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import PiwikPro from '@piwikpro/react-piwik-pro';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    PiwikPro.initialize('b2da65e5-4cfd-47f6-ac5c-934e1bc8551e', 'https://pulsybkr.containers.piwik.pro');
+  }, [])
   return <Component {...pageProps} />
 }
